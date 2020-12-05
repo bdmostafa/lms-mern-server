c12eebc11748b09370256410247877f82b7ae4da
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-12-06 00:13:22 +0600

    get route

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
 const express = require('express');
 const app = express();
 
+const books = [
+    {
+        id: 1,
+        name: "abc",
+        author: "xyz"
+    },
+    {
+        id: 1,
+        name: "abc",
+        author: "xyz"
+    }
+    
+]
+
 
 app.get('/', (req, res) => {
     res.send('I am from LMS (Library Management System')
 })
 
+app.get('/books', (req, res) => {
+    if (books.length == 0) {
+        return res.send('No book')
+    }
+    res.status(200).send(Books)
+})
 
+app.get('/books/:bookId', (req, res) => {
+    const id = req.params.bookId;
+    const book = books.find(book => book.id === id)
+    if (book) return res.send(book);
+    res.status(404).send('Books Not Found');
+})
+
+
+
+
+app.get('*', (req, res) => {
+    res.status(404).send('404 Not Found')
+})
 
 
 
